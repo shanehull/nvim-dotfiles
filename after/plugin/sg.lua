bec8a8fa6e3a6fd5dcd7b0ac9f1606b3f1506b64
shanehull <shane.hull@annalise.ai>
2024-01-22 13:06:00 +1100

    fix ai assist logic

--- after/plugin/sg.lua
+++ after/plugin/sg.lua
@@ -1,5 +1,13 @@
-local enable_cody = vim.fn.getenv("ALLOW_AI_ASSIST")
+if vim.fn.filereadable(vim.fn.expand("~/.config/nvim/env.lua")) == 1 then
+	vim.cmd("source ~/.config/nvim/env.lua")
+end
 
-require("sg").setup({
-	enable_cody = enable_cody,
-})
+local enable_cody = vim.env.ALLOW_AI_ASSIST == "v:true"
+
+-- TODO: Revisit this 'enable_cody = false' breaks everything with:
+--       Invalid command (not found): CodyTaskView
+if enable_cody then
+	require("sg").setup({
+		enable_cody = enable_cody,
+	})
+end
