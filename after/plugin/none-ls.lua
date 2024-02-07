b405fd016c2c640d90d4db4532b13596de01e212
shanehull <hello@shanehull.com>
2024-02-08 08:57:40 +1100

    chore: format and diag for nix

--- after/plugin/none-ls.lua
+++ after/plugin/none-ls.lua
@@ -7,6 +7,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.goimports,
 		null_ls.builtins.formatting.golines,
 		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.alejandra,
 		null_ls.builtins.formatting.prettierd.with({
 			filetypes = {
 				"javascript",
@@ -22,6 +23,7 @@ null_ls.setup({
 		}),
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.diagnostics.golangci_lint,
+		null_ls.builtins.diagnostics.statix,
 		null_ls.builtins.completion.spell,
 		null_ls.builtins.code_actions.eslint_d,
 	},
