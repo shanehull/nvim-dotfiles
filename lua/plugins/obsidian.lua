bd75407830639cb4b336480fe061e84cf16a6404
shanehull <shane.hull@annalise.ai>
2024-02-26 10:33:49 +1100

    chore: obsidian only in notes dir

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,13 +2,13 @@ return {
 	"epwalsh/obsidian.nvim",
 	version = "*", -- use latest release instead of latest commit
 	lazy = true,
-	ft = { "markdown" },
-	--event = {
-	--"BufReadPre " .. vim.fn.expand("~") .. "/secondbrain/**/**.md",
-	--"BufNewFile " .. vim.fn.expand("~") .. "/secondbrain/**/**.md",
-	----"BufReadPre " .. vim.fn.expand("$SECOND_BRAIN") .. "/**/**.md",
-	----"BufNewFile " .. vim.fn.expand("$SECOND_BRAIN") .. "/**/**.md",
-	--},
+	--ft = { "markdown" },
+	event = {
+		"BufReadPre " .. vim.fn.expand("~") .. "/secondbrain/**/**.md",
+		"BufNewFile " .. vim.fn.expand("~") .. "/secondbrain/**/**.md",
+		--"BufReadPre " .. vim.fn.expand("$SECOND_BRAIN") .. "/**/**.md",
+		--"BufNewFile " .. vim.fn.expand("$SECOND_BRAIN") .. "/**/**.md",
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
